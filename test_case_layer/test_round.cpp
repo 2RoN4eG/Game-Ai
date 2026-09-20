de88dcf8afd08dfefb21dea9cf1ed98d8171305c
Mikalai Bohdan <2RoN4eG@gmail.com>
2026-09-20 17:58:48 +0300

    test

--- test_case_layer/test_round.cpp
+++ test_case_layer/test_round.cpp
@@ -6,6 +6,30 @@
 
 TEST_CASE( "round" )
 {
+    SECTION( "testing 'long double round(long double)'" )
+    {
+        struct t_testable {
+            long double value {};
+            long double expected {};
+        };
+
+        std::vector<t_testable> testables {
+            { 0.4l, 0.0l },
+            { 0.5l, 1.0l },
+            { 1.0l, 1.0l },
+            { 1.1l, 1.0l },
+            { 1.5l, 2.0l }
+        };
+
+        for (const t_testable& testable : testables) {
+            const long double value = testable.value;
+            const long double expected = testable.expected;
+            const long double rounded = std::round(value);
+
+            REQUIRE(rounded == expected);
+        }
+    }
+
     SECTION( "testing 'double round(double)'" )
     {
         struct t_testable {
@@ -25,6 +49,54 @@ TEST_CASE( "round" )
             const double value = testable.value;
             const double expected = testable.expected;
             const double rounded = std::round(value);
+
+            REQUIRE(rounded == expected);
+        }
+    }
+
+    SECTION( "testing 'float round(float)'" )
+    {
+        struct t_testable {
+            float value {};
+            float expected {};
+        };
+
+        std::vector<t_testable> testables {
+            { 0.4f, 0.0f },
+            { 0.5f, 1.0f },
+            { 1.0f, 1.0f },
+            { 1.1f, 1.0f },
+            { 1.5f, 2.0f }
+        };
+
+        for (const t_testable& testable : testables) {
+            const float value = testable.value;
+            const float expected = testable.expected;
+            const float rounded = std::round(value);
+
+            REQUIRE(rounded == expected);
+        }
+    }
+
+    SECTION( "testing 'long double roundl(long double)'" )
+    {
+        struct t_testable {
+            long double value {};
+            long double expected {};
+        };
+
+        std::vector<t_testable> testables {
+            { 0.4l, 0.0l },
+            { 0.5l, 1.0l },
+            { 1.0l, 1.0l },
+            { 1.1l, 1.0l },
+            { 1.5l, 2.0l }
+        };
+
+        for (const t_testable& testable : testables) {
+            const long double value = testable.value;
+            const long double expected = testable.expected;
+            const long double rounded = std::roundl(value);
 
             REQUIRE(rounded == expected);
         }
